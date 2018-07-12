2c5007927d6dae3b23a319dc513293d56bf2b359
DaiJing <dieking2000@gmail.com>
2018-07-12 13:28:50 +0800

    Update index.js
    support echarts registerMap，set webview  scrollEnabled to false

--- index.js
+++ index.js
@@ -6,27 +6,30 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { View, WebView, Platform } from 'react-native'
 
-const renderChart = ({ height, option }) => `
+const renderChart = ({ height, option, geoJson }) => `
   var main = document.getElementById('main');
   main.style.height = "${height}px";
   var myChart = echarts.init(document.getElementById('main'));
+  if (${JSON.stringify(geoJson)} != {}){
+    echarts.registerMap('geoJson', ${JSON.stringify(geoJson)});
+  }
   myChart.setOption(${JSON.stringify(option)});
-`
+  `
 
 export default class Echarts extends React.Component {
   componentWillReceiveProps (nextProps) {
-    if (JSON.stringify(this.props.option) !== JSON.stringify(nextProps.option) || this.props.height !== nextProps.height) {
-      this.echart.injectJavaScript(renderChart({ height: nextProps.height, option: nextProps.option }))
+    if (JSON.stringify(this.props.option) !== JSON.stringify(nextProps.option) || this.props.height !== nextProps.height || JSON.stringify(this.props.geoJson) !== JSON.stringify(nextProps.geoJson)) {
+      this.echart.injectJavaScript(renderChart({ height: nextProps.height, option: nextProps.option, geoJson: nextProps.geoJson}))
     }
   }
 
   render () {
-    const { height, width, style, source, option, onLoadStart, onLoad, onError, onLoadEnd, onMessage, renderLoading, renderError } = this.props
+    const { height, width, style, source, option, onLoadStart, onLoad, onError, onLoadEnd, onMessage, renderLoading, renderError,geoJson } = this.props
     return <View style={{width, height}}>
       <WebView
         ref={node => { this.echart = node }}
         style={[style, {height, width, backgroundColor: 'transparent'}]}
-        injectedJavaScript={renderChart({ height, option })}
+        injectedJavaScript={renderChart({ height, option, geoJson })}
         source={source ? source : Platform.OS === 'android' && !__DEV__ ? { uri:'file:///android_asset/tpl.html' } : require('./tpl.html')}
         javaScriptEnabled={true}
         domStorageEnabled={true}
@@ -40,6 +43,7 @@ export default class Echarts extends React.Component {
         onMessage={onMessage}
         renderLoading={renderLoading}
         renderError={renderError}
+        scrollEnabled={false}
       />
     </View>
   }
@@ -57,6 +61,7 @@ Echarts.propTypes = {
   style: PropTypes.object,
   source: PropTypes.object,
   option: PropTypes.object.isRequired,
+  geoJson: PropTypes.object,  
   onLoadStart: PropTypes.func,
   onLoad: PropTypes.func,
   onError: PropTypes.func,
@@ -68,5 +73,6 @@ Echarts.propTypes = {
 
 Echarts.defaultProps = {
   width: 300,
-  height: 300
+  height: 300,
+  geoJson: {}
 }
